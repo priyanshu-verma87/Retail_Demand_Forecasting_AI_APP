4810ae961e97901fc0c930fec34c2b6961aa6fc8
Priyanshu Verma <priyanshuvsp2841@gmail.com>
2026-04-28 17:32:51 +0530

    Fixing some errors

--- dags/retail_demand_pipeline.py
+++ dags/retail_demand_pipeline.py
@@ -19,7 +19,7 @@ with DAG(
     default_args=default_args,
     description="Store Item Demand Forecasting MLOps Pipeline",
     start_date=datetime(2026, 2, 2),
-    schedule_interval="@daily",
+    schedule=None,
     catchup=False,
     tags=["forecasting", "retail", "mlops"]
 ) as dag:
